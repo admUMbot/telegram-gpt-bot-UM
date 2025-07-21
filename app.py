cc45b31e8f346d7c56fd66749019e573c0510bb3
admUMbot <matveygpterm@gmail.com>
2025-07-21 18:16:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,8 @@ def chat():
 def index():
     return send_from_directory("static", "index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
 
